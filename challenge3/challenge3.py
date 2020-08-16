669a08c9b66ef3fe579afce2b19dd6a657655109
Scott Hansen <scotthansenpaysoneast@gmail.com>
2020-08-15 21:19:20 -0600

    Challenge 3 is created and c2 was fixed.

--- challenge3/challenge3.py
+++ challenge3/challenge3.py
@@ -2,7 +2,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class Challenge2(unittest.TestCase):
+class Challenge3(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome("../venv/chromedriver")
@@ -10,15 +10,9 @@ class Challenge2(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
 
-    def test_challenge2(self):
+    def test_challenge3(self):
         self.driver.get("https://www.copart.com")
         self.assertIn('Copart', self.driver.title)
-        searchbox = self.driver.find_element_by_id('input-search')
-        searchbox.send_keys("exotics")
-        searchbox.send_keys(Keys.RETURN)
-        self.driver.implicitly_wait(5)
-        makeItem = self.driver.find_element_by_xpath("//tr[1]//td[5]//span[1]")
-        self.assertIn('PORSCHE', makeItem.text)
 
 
 if __name__ == '__main__':

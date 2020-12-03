ccc7ffd94301723070657e2b996cff069d71e24d
Scott Hansen <scotthansenpaysoneast@gmail.com>
2020-12-02 18:10:58 -0700

    Challenge 3 is done.

--- challenge3/challenge3.py
+++ challenge3/challenge3.py
@@ -1,6 +1,7 @@
 import unittest
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
 
 class Challenge3(unittest.TestCase):
 
@@ -13,6 +14,11 @@ class Challenge3(unittest.TestCase):
     def test_challenge3(self):
         self.driver.get("https://www.copart.com")
         self.assertIn('Copart', self.driver.title)
+        driver_wait = WebDriverWait(self.driver, 20)
+
+        vehicles = self.driver.find_elements_by_css_selector("#tabTrending > div > div > div > ul > li > a")
+        for vehicle in vehicles:
+            print( str(vehicle.get_attribute('title')) + str(vehicle.get_attribute('text')) + " - " + str(vehicle.get_attribute('href')))
 
 
 if __name__ == '__main__':
